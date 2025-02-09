1a45fbed15fd5e547f39ef7b6407aa170eb1acf0
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-02-09 09:02:58 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -109,7 +109,10 @@ public:
 class Menu {
 private:
     std::vector<UseCase*> useCases;
+    DaoManager* daoManager;
 public:
+    Menu(DaoManager* daoManager) : daoManager(daoManager){}
+
     void addUseCase(UseCase* useCase) {
         useCases.push_back(useCase);
     }
@@ -119,17 +122,49 @@ public:
         for (size_t i = 0; i < useCases.size(); ++i) {
             std::cout << i + 1 << ". " << useCases[i]->getName() << std::endl;
         }
+        std::cout << useCases.size() + 1 << ". Mostrar todos os pedidos" << std::endl;
         std::cout << "0. Sair" << std::endl;
     }
 
     void executeUseCase(int choice) {
         if (choice > 0 && choice <= useCases.size()) {
             useCases[choice - 1]->execute();
+        } else if (choice == useCases.size() + 1) {
+            mostrarTodosOsPedidos();
         } else {
             std::cout << "Escolha inválida" << std::endl;
         }
     }
+
+    void mostrarTodosOsPedidos() {
+        std::vector<Pedido> pedidos = daoManager->getPedidoDao()->getAllPedidos();
+        for (const Pedido& pedido : pedidos) {
+            std::cout << "Pedido ID: " << pedido.getId() << ", Descrição: " << pedido.getDescricao() << ", Situação: " << pedido.getSituacao() << std::endl;
+        }
+    }
 };
+
+void setup(DaoManager* daoManager) {
+    Cliente cliente1("Cliente 1", "12345678901", "Endereco 1", "Telefone 1", "email1@example.com", CategoriaCliente::PESSOA_FISICA);
+    Cliente cliente2("Cliente 2", "98765432109", "Endereco 2", "Telefone 2", "email2@example.com", CategoriaCliente::PESSOA_JURIDICA);
+    daoManager->getClienteDao()->adicionarCliente(cliente1);
+    daoManager->getClienteDao()->adicionarCliente(cliente2);
+
+    Pedido pedido1(1, std::time(nullptr), "Descricao Pedido 1", Situacao::ABERTO, "12345678901");
+    Pedido pedido2(2, std::time(nullptr), "Descricao Pedido 2", Situacao::ABERTO, "98765432109");
+    daoManager->getPedidoDao()->addPedido(pedido1);
+    daoManager->getPedidoDao()->addPedido(pedido2);
+
+    Pagamento pagamento1(1, 100.0f, std::time(nullptr), FormaPagamento::DINHEIRO, SituacaoPagamento::QUITADO);
+    Pagamento pagamento2(2, 200.0f, std::time(nullptr), FormaPagamento::CREDITO, SituacaoPagamento::EM_HAVER);
+    daoManager->getPagamentoDao()->adicionarPagamento(pagamento1);
+    daoManager->getPagamentoDao()->adicionarPagamento(pagamento2);
+
+    NotaFiscal notaFiscal1(1, std::time(nullptr), 1);
+    NotaFiscal notaFiscal2(2, std::time(nullptr), 2);
+    daoManager->getNotaFiscalDao()->addNotaFiscal(notaFiscal1);
+    daoManager->getNotaFiscalDao()->addNotaFiscal(notaFiscal2);
+}
 
 int main() {
     DaoManager daoManager;
@@ -137,7 +172,9 @@ int main() {
     PedidoManager pedidoManager(&daoManager, &dvoManager);
     ClienteManager clienteManager(&daoManager, &dvoManager);
 
-    Menu menu;
+    setup(&daoManager);
+
+    Menu menu(&daoManager);
     menu.addUseCase(new RealizarPedido(&pedidoManager, &clienteManager));
     menu.addUseCase(new RealizarPagamento(&pedidoManager, &clienteManager));
     menu.addUseCase(new AtualizarSituacaoPedido());
